64a0fd1f3dc6a2d52fbccb65da2ee8bde407d3c8
Danil <46310990+Don1k1337@users.noreply.github.com>
2021-06-23 15:06:24 +0600

    Layout improved, localStorage added, API user-side functionality improved.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
-const weatherApiKey = '99be4880848478fc68a6f9d53e3a1f6e';
-const weatherApiPath = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${weatherApiKey}`
-fetch(weatherApiPath)
-    .then(resp => console.log(resp.json()));
+let weather = {
+    weatherApiKey: '99be4880848478fc68a6f9d53e3a1f6e',
+    fetchWeather(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.weatherApiKey
+        )
+            .then((resp) => resp.json())
+            .then((data) => localStorage.setItem((data.toString()), JSON.stringify(this.displayWeather(data))))
+            .catch(() => {
+                alert("No such city/country found, please enter the valid information to the search-box")
+            })
+    },
 
+    searchWeather() {
+        this.fetchWeather(document.querySelector('.search-bar').value);
+    },
+    displayWeather: data => {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+
+        document.querySelector('.city').innerHTML = "Current weather in " + name;
+        document.querySelector('.weather__desc').innerText = description;
+        document.querySelector('.icon').src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector('.temp').innerText = temp + "°C";
+        document.querySelector('.humidity').innerText = "Humidity feels like: " + humidity + "%";
+        document.querySelector('.wind').innerText = "Wind speed: " + speed + " km/h";
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        document.querySelector(".weather-container").classList.remove("loading");
+    }
+};
+
+document.querySelector('.search-bar__container button').addEventListener('click', () => {
+    weather.searchWeather();
+    const objToLS = {
+        city: document.querySelector('.city').innerHTML,
+        temp: document.querySelector('.temp').innerHTML,
+        description: document.querySelector('.weather__desc').innerHTML
+    };
+    localStorage.setItem('city', JSON.stringify(objToLS))
+})
+
+document.querySelector('.search-bar').addEventListener('keyup', (e) => {
+    if(e.key === "Enter") {
+        weather.searchWeather();
+    }
+})
